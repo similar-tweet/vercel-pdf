262f5dae6d4844457c1ef3163b834ef6929373c4
similar-tweet <74700452+similar-tweet@users.noreply.github.com>
2020-12-03 17:28:41 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const renderPdf = async (name) => {
   const context = await browser.newContext()
   const page = await context.newPage()
 
-  await page.goto(`https://project1-65dnl1uco.vercel.app/main.html?name=${name}`)
+  await page.goto(`https://twitter.com`)
   await page.emulateMedia('screen')
   const pdf = await page.pdf({
     format: 'A4',

096b607faadea391c9a230fde8aaa249078b5c9c
similar-tweet <74700452+similar-tweet@users.noreply.github.com>
2020-12-03 21:04:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const renderPdf = async (name, url) => {
   const context = await browser.newContext({
     userAgent: UA })
   const page = await context.newPage()
-  await page.goto(url || `https://twitter.com`, { waitUntil: 'domcontentloaded' })
+  await page.goto(url || `https://twitter.com`, { waitUntil: 'networkidle' })
   const html = await page.$eval('html', e => e.outerHTML);
 
   return html;
